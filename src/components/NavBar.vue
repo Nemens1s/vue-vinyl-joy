b03ba1a63f9ac91bdac841375e3b1407bdd0f598
Ilja <ilniko@taltech.ee>
2020-04-13 16:56:59 +0300

    added navigation bar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <b-navbar id="my_navbar" toggleable="lg" class="">
+            <b-navbar-brand>
+                <div class="logo ml-2">
+                    Vinyl J<span><img id="vinyl-record" src="@/images/vinyl-record.svg"
+                                                  alt="vinyl"></span>y
+                    <p>Keep those records spinning</p>
+                </div>
+            </b-navbar-brand>
+
+            <b-navbar-toggle target="nav-collapse">
+                <template>
+                    <b-icon icon="list" style="color: #E5E5E5" font-scale="2"></b-icon>
+                </template>
+            </b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+                <!-- Right aligned nav items -->
+                <b-navbar-nav class="ml-auto nav__links text-right">
+                    <b-nav-item>CATALOGUE</b-nav-item>
+                    <b-nav-item>CART</b-nav-item>
+                </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+    </div>
+</template>
+
+
+<script>
+    require("@/css/common.css");
+</script>
