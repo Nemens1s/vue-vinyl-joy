858bfd67e6fa271a3f3a3043fde2c30ebcc27521
Ilja <ilniko@taltech.ee>
2020-04-15 18:46:28 +0300

    fixed style, added axios and fake data

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@
 
 <script>
 import NavBar from "@/components/NavBar"
+import axios from 'axios'
 export default {
   name: 'App',
   components: {
@@ -23,33 +24,16 @@ export default {
       }
     }
   },
+  created(){
+    axios.get('data.json')
+            .then(response => {
+              this.records = response.data
+            })
+  },
   data() {
     return{
       cart : [],
-      records: [
-        {
-          'id': 1,
-          'title' : 'De Mysteriis Dom Sathanas',
-          'band' : 'Mayhem',
-          'price' : 20,
-          'url' : "https://upload.wikimedia.org/wikipedia/ru/c/ce/De_Mysteriis_Dom_Sathanas.jpg"
-        },
-        {
-          'id' : 2,
-          'title': 'Sergeant Pepper',
-          'band' : 'The Beatles',
-          'price' : 25,
-          'url' : "https://d8mkdcmng3.imgix.net/3782/586566.png?auto=format&bg=0FFF&fit=fill&h=600&q=100&w=600&s=d1b119d71f592980ec46b929d11ae48c"
-        },
-        {
-          'id' : 2,
-          'title': 'Sergent Pepper',
-          'band' : 'The Beatles',
-          'price' : 25,
-          'url' : "https://d8mkdcmng3.imgix.net/3782/586566.png?auto=format&bg=0FFF&fit=fill&h=600&q=100&w=600&s=d1b119d71f592980ec46b929d11ae48c"
-        },
-
-      ],
+      records: [],
     }
   }
 
